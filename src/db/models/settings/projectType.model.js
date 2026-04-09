a378f11c1490ada99889ee18dc96d35047d6c6d3
Amrmoo2004 <amrmohamad489@gmail.com>
2026-04-09 07:47:12 +0200

    feat: Update phaseFieldSchema to include 'textarea' as a valid type option

--- src/db/models/settings/projectType.model.js
+++ src/db/models/settings/projectType.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const phaseFieldSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
-        type: { type: String, enum: ['text', 'number', 'date', 'file'], default: 'text' },
+        type: { type: String, enum: ['text', 'textarea', 'number', 'date', 'file'], default: 'text' },
         isRequired: { type: Boolean, default: false }
     },
     { _id: false }
